62348f9febc21ea3e810713dbe348c178df6f503
Riva Almero <riva@dev.netkromsolution.com>
2024-12-05 15:55:57 +0700

    feat: expose type to extending project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
    devtools: { enabled: false },
 
+   hooks: {
+      'prepare:types'(ctx) {
+         ctx.references.push({
+            path: './types/index.d.ts'
+         })
+      }
+   },
+
    runtimeConfig: {
       apiUrl: process.env.NUXT_API_URL || ''
    }
